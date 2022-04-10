8cc73b2aeee0db4901ffdd0d6401141f81c58d70
jiwonjung <4dakto@gmail.com>
2022-04-10 20:37:14 +0900

    test

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   repositoryUrl: "https://github.com/wilson-jung/github-action-test",
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
   ],
 };
