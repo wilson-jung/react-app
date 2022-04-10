c94cc1a763068d42206cb2be27f10c46d0c2bc8f
jiwonjung <4dakto@gmail.com>
2022-04-10 17:45:02 +0900

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/wilson-jung/github-action-test",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
